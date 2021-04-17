789a67393f0b0f6844345395979faaeb32159adc
dhairya2711 <dsdrago27@gmail.com>
2021-04-17 01:05:36 -0700

    changes

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)d*o&i+%_mg@b-b30*(oun0klxp6nnh301n&8(l$ufke8%gnfs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["tranquil-caverns-78963.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
